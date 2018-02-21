221e93334bb0375be4817daa2ffb54ba0e9b62dc
Silence <daniel.s.mo503@gmail.com>
2018-02-21 21:15:19 +0800

    首页加上logo与比赛倒计时

--- CTFd/views.py
+++ CTFd/views.py
@@ -38,22 +38,7 @@ def setup():
 
             # Index page
 
-            index = """<div class="row">
-    <div class="col-md-6 offset-md-3">
-        <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="themes/core/static/img/logo.png" />
-        <h3 class="text-center">
-            <p>A cool CTF platform from <a href="https://ctfd.io">ctfd.io</a></p>
-            <p>Follow us on social media:</p>
-            <a href="https://twitter.com/ctfdio"><i class="fab fa-twitter fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://facebook.com/ctfdio"><i class="fab fa-facebook fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://github.com/ctfd"><i class="fab fa-github fa-2x" aria-hidden="true"></i></a>
-        </h3>
-        <br>
-        <h4 class="text-center">
-            <a href="admin">Click here</a> to login and setup your CTF
-        </h4>
-    </div>
-</div>""".format(request.script_root)
+            index = """<div id="rC_PE" class="redCountdownDemo"></div>""".format(request.script_root)
 
             page = Pages(title=None, route='index', html=index, draft=False)
 
